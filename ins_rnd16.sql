e1251a9480f0e0d562271e41a04d660815c79a3e
Juan C Marin <jcmarinn@gmail.com>
2018-05-18 23:03:20 -0400

    Script rnd 16

---
+++ ins_rnd16.sql
@@ -0,0 +1,11 @@
+INSERT INTO games 
+(`id`,`round`,`team1_id`,`team2_id`,`date`,`stadium`,`goal1`,`goal2`) 
+VALUES 
+(49,'Round of 16',9,14,'2018-06-30 10:00:00','Kazan Arena',NULL,NULL),
+(50,'Round of 16',1,6,'2018-06-30 14:00:00','Fisht Stadium',NULL,NULL),
+(51,'Round of 16',5,2,'2018-07-01 10:00:00','Luzhniki Stadium',NULL,NULL),
+(52,'Round of 16',13,10,'2018-07-01 14:00:00','Nizhny Novgorod Stadium',NULL,NULL),
+(53,'Round of 16',17,21,'2018-07-02 10:00:00','Samara Arena',NULL,NULL),
+(54,'Round of 16',25,30,'2018-07-02 14:00:00','Rostov Arena',NULL,NULL),
+(55,'Round of 16',21,23,'2018-07-03 10:00:00','Saint Petersburg Stadium',NULL,NULL),
+(56,'Round of 16',29,26,'2018-07-03 14:00:00','Spartak Stadium',NULL,NULL)
