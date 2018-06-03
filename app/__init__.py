cf271dc858152d200e59d242f993b5532ecce411
Juan C Marin <jcmarinn@gmail.com>
2018-06-03 18:54:50 -0400

    changes to days left

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-import logging, datetime
+import logging
+from datetime import datetime
 from flask import Flask, render_template, g
 from flask_appbuilder import SQLA, AppBuilder, BaseView, expose, has_access
 from app.index import JCIndexView
@@ -18,11 +19,19 @@ appbuilder = AppBuilder(app, db.session, indexview=JCIndexView)
 from functions import add_records, calc_stand, add_UsrScores, limit, has_changed
 from app import views
 
+@app.context_processor
+def todays():
+    now=datetime.now()
+    last_day=datetime(2018,6,14)
+    dif=last_day-now
+    return dict(dif=str(dif.days))
+
+app.jinja_env.globals.update(todays=todays)
+
 @app.before_request
 def load_users():
     if current_user.is_authenticated():
         usr = current_user.get_id() # return username in get_id()
-        now=datetime.datetime.now()
         add_records(usr)
         add_UsrScores(usr)
         has_changed(usr)
--- app/index.py
+++ app/index.py
@@ -1,9 +1,5 @@
-from datetime import datetime, timedelta
+
 from flask_appbuilder import IndexView
 
 class JCIndexView(IndexView):
-    now=datetime.now()
-    last_day=datetime(2018,6,14)
-    dif=last_day-now
     index_template = 'index.html'
-    extra_args = {'today':'Only '+str(dif.days)+' days left - make sure you input your predictions and deposit your $20'}
